56f5e3e8afb0f191963ffcc7665bd1217b465cfc
Ruslan <37127343+RusS1103@users.noreply.github.com>
2019-09-21 17:03:39 +0300

    Create Пишем свой вектор.cpp

---
+++ Неделя 3/Пишем свой вектор.cpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <cstdlib>
+
+template <typename T>
+class SimpleVector {
+public:
+    SimpleVector() {
+        size_ = 0u;
+        capacity_ = 0u;
+        data = nullptr;
+        end_ = nullptr;
+    }
+    explicit SimpleVector(size_t size) {
+        data = new T[size];
+        end_ = data + size;
+        size_ = size;
+        capacity_ = size;
+    }
+    ~SimpleVector() {
+        delete[] data;
+    }
+    
+    T& operator[](size_t index) {
+        return data[index];
+    }
+    
+    T* begin() {
+        return data;
+    }
+    T* end() {
+        return end_;
+    }
+    
+    const T* begin() const {
+        return data;
+    }
+    const T* end() const {
+        return end_;
+    }
+    
+    size_t Size() const {
+        return size_;
+    }
+    size_t Capacity() const {
+        return capacity_;
+    }
+    
+    void PushBack(const T& value) {
+        if (size_ == 0u) {
+            data = new T[++capacity_];
+        } else if (Size() == Capacity()) {
+            T* new_data = data;
+            capacity_ *= 2;
+            data = new T[capacity_];
+            for (size_t i = 0; i < Size(); ++i) {
+                data[i] = new_data[i];
+            }
+            delete[] new_data;
+        }
+        data[size_++] = value;
+        end_ = data + size_;
+    }
+    
+private:
+    T* data;
+    T* end_;
+    size_t size_ = 0u;
+    size_t capacity_ = 0u;
+};
