a8c523835fccffb789e74df045975b7a28c18843
Ruslan <37127343+RusS1103@users.noreply.github.com>
2019-10-17 18:14:40 +0300

    Update Исследование блогов.cpp

--- Неделя 5/Исследование блогов.cpp
+++ Неделя 5/Исследование блогов.cpp
@@ -1,175 +1,118 @@
 #include "test_runner.h"
 #include "profile.h"
-
+ 
 #include <map>
 #include <string>
 #include <future>
+ 
 using namespace std;
-
-template <typename Iterator>
-class IteratorRange {
-public:
-  IteratorRange(Iterator begin, Iterator end)
-    : first(begin)
-    , last(end)
-    , size_(distance(first, last))
-  {
-  }
-
-  Iterator begin() const {
-    return first;
-  }
-
-  Iterator end() const {
-    return last;
-  }
-
-  size_t size() const {
-    return size_;
-  }
-
-private:
-  Iterator first, last;
-  size_t size_;
-};
-
-template <typename Iterator>
-class Paginator {
-private:
-  vector<IteratorRange<Iterator>> pages;
-
-public:
-  Paginator(Iterator begin, Iterator end, size_t page_size) {
-    for (size_t left = distance(begin, end); left > 0; ) {
-      size_t current_page_size = min(page_size, left);
-      Iterator current_page_end = next(begin, current_page_size);
-      pages.push_back({begin, current_page_end});
-
-      left -= current_page_size;
-      begin = current_page_end;
-    }
-  }
-
-  auto begin() const {
-    return pages.begin();
-  }
-
-  auto end() const {
-    return pages.end();
-  }
-
-  size_t size() const {
-    return pages.size();
-  }
-};
-
-template <typename C>
-auto Paginate(C& c, size_t page_size) {
-  return Paginator(begin(c), end(c), page_size);
-}
-
+ 
 struct Stats {
-    map<string, int> word_frequences;
-
-    void operator += (const Stats& other) {
-        map<string, int> other_map = other.word_frequences;
-        for (auto now : other_map) {
-            word_frequences[now.first] += other_map[now.first];
-        }
-    }
-};
-
-Stats ExploreLine(const set<string>& key_words, const string& line) {
-    Stats stats;
-    istringstream ss(line);
-    string s;
-    map<string, int> dict;
-    while (ss >> s) {
-        ++dict[s];
-    }
-    map<string, int> stat_words;
-    for (auto& now : key_words) {
-        stat_words[now] = dict[now];
-    }
-    stats.word_frequences = stat_words;
-    for (auto now : key_words) {
-        if (stats.word_frequences[now] == 0) {
-            stats.word_frequences.erase(now);
-        }
-    }
-    return stats;
-}
-
-Stats ExploreKeyWordsSingleThread(
-  const set<string>& key_words, istream& input
-) {
-  Stats result;
-  for (string line; getline(input, line); ) {
-    result += ExploreLine(key_words, line);
+  map<string, int> word_frequences;
+ 
+  void operator += (const Stats& other) {
+      map<string, int> other_map = other.word_frequences;
+      for (auto now : other_map) {
+          word_frequences[now.first] += other_map[now.first];
+      }
   }
-  return result;
+};
+ 
+    
+Stats Explore(const vector<string>& vector_words,
+                const set<string>& key_words) {
+    Stats result;
+    map<string, int> new_map;
+    for (auto now : vector_words) {
+        ++new_map[now];
+    }
+    for (auto now : key_words) {
+        if (new_map[now] != 0) {
+            result.word_frequences[now] = new_map[now];
+        }
+    }
+    return result;
 }
-
-
-vector<set<string>> Pag(const set<string>& key_words, size_t page_size) {
-    vector<set<string>> pages;
-    auto begin = key_words.begin();
-    for (size_t left = distance(begin, key_words.end()); left > 0; ) {
+ 
+ 
+vector<string> GetVector(istream& input) {
+    vector<string> result;
+    for (string line; getline(input, line); ) {
+        istringstream ss(line);
+        string s;
+        while (ss >> s) {
+            result.push_back(s);
+        }
+    }
+    return result;
+}
+   
+vector<vector<string>> Pag(const vector<string>& lines, size_t page_size) {
+    vector<vector<string>> pages;
+    auto begin = lines.begin();
+    for (size_t left = distance(begin, lines.end()); left > 0; ) {
       size_t current_page_size = min(page_size, left);
       auto current_page_end = next(begin, current_page_size);
       pages.push_back({begin, current_page_end});
-
+ 
       left -= current_page_size;
       begin = current_page_end;
     }
     return pages;
 }
 
+vector<vector<string>> PagOnThreads(const vector<string>& lines, size_t threads) {
+    size_t page_size = lines.size() % threads == 0 ? lines.size() / threads : lines.size() / threads + 1;
+    vector<vector<string>> pages;
+       auto begin = lines.begin();
+       for (size_t left = distance(begin, lines.end()); left > 0; ) {
+         size_t current_page_size = min(page_size, left);
+         auto current_page_end = next(begin, current_page_size);
+         pages.push_back({begin, current_page_end});
+    
+         left -= current_page_size;
+         begin = current_page_end;
+       }
+    return pages;
+}
+
+
+ 
 Stats ExploreKeyWords(const set<string>& key_words, istream& input) {
     Stats result;
+    vector<string> lines = GetVector(input);
     vector<future<Stats>> futures;
-    for (auto page : Pag(key_words, 2000)) {
-        futures.push_back(async([&page, &input]() {
-            return ExploreKeyWordsSingleThread(page, input);}));
+    vector<vector<string>> Pags = PagOnThreads(lines, 4);
+    for (size_t i = 0; i < 4; ++i) {
+        vector<string> temp_v = Pags[i];
+        futures.push_back(async(Explore, ref(temp_v), ref(key_words)));
     }
     for (auto& now : futures) {
         result += now.get();
     }
     return result;
 }
-
+ 
 void TestBasic() {
-  const set<string> key_words = {"yangle", "rocks", "sucks", "all"};
-
-  stringstream ss;
-  ss << "this new yangle service really rocks\n";
-  ss << "It sucks when yangle isn't available\n";
-  ss << "10 reasons why yangle is the best IT company\n";
-  ss << "yangle rocks others suck\n";
-  ss << "Goondex really sucks, but yangle rocks. Use yangle\n";
-
-  const auto stats = ExploreKeyWords(key_words, ss);
-  const map<string, int> expected = {
-    {"yangle", 6},
-    {"rocks", 2},
-    {"sucks", 1}
-  };
-  ASSERT_EQUAL(stats.word_frequences, expected);
+    const set<string> key_words = {"yangle", "rocks", "sucks", "all"};
+ 
+    stringstream ss;
+    ss << "this new yangle service really rocks\n";
+    ss << "It sucks when yangle isn't available\n";
+    ss << "10 reasons why yangle is the best IT company\n";
+    ss << "yangle rocks others suck\n";
+    ss << "Goondex really sucks, but yangle rocks. Use yangle\n";
+ 
+    const auto stats = ExploreKeyWords(key_words, ss);
+    const map<string, int> expected = {
+        {"yangle", 6},
+        {"rocks", 2},
+        {"sucks", 1}
+    };
+    ASSERT_EQUAL(stats.word_frequences, expected);
 }
-
+ 
 int main() {
-//    vector<set<string>> se = Pag({"a", "b", "c"}, 200);
-//    cout << se.size();
     TestRunner tr;
     RUN_TEST(tr, TestBasic);
 }
-    
-//    for (string line; getline(input, line); ) {
-//        result += ExploreLine(key_words, line);
-//    }
-//    for (auto now : result.word_frequences) {
-//        if (now.second == 0) {
-//            result.word_frequences.erase(now.first);
-//        }
-//    }
-//    return result;
